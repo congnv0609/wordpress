2da539f81942bb6ad7f097d62164b8b9a9b457b5
EC2 Default User <ec2-user@ip-172-31-20-15.ap-southeast-1.compute.internal>
2024-01-27 09:47:42 +0000

    add bash script

---
+++ renew-certification.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+docker-compose -f compose-production.yaml run --rm certbot renew
+# docker-compose -f compose-production.yaml run --rm certbot certonly --webroot --webroot-path /var/www/certbot/ --email congnv69@gmail.com --agree-tos --no-eff-email -d hccomputer.net -d www.hccomputer.net
