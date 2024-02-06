f532e3f52a908af99ac967893f0b9c39552a5f40
EC2 Default User <ec2-user@ip-172-31-3-96.ap-southeast-1.compute.internal>
2024-02-06 02:28:46 +0000

    last change

--- get-certification.sh
+++ get-certification.sh
@@ -1,4 +1,17 @@
 #!/bin/sh
 
 #docker-compose -f compose-production.yaml run --rm certbot renew
-docker-compose -f compose-production.yaml run --rm certbot certonly --webroot --webroot-path /var/www/certbot/ --email congnv69@gmail.com --agree-tos --no-eff-email -d hccomputer.net -d www.hccomputer.net
+#docker-compose -f compose-production.yaml run --rm certbot certonly --webroot --webroot-path /var/www/certbot/ --email congnv69@gmail.com --agree-tos --no-eff-email -d hccomputer.net -d www.hccomputer.net
+
+docker-compose -f compose-production.yaml run --rm certbot register \
+    --email "congnv69@gmail.com" \
+    --no-eff-email \
+    --server "https://dv.acme-v02.api.pki.goog/directory" \
+    --eab-kid "cac98befe3201d595b82a89e03491785" \
+    --eab-hmac-key "ZxwxQp2YcE12yBfEHmvPiPEJvO5e8gSfLcZJp_J_fPZd-5q5OkH0KSKcwlzNZOLrCprwnRjO1xjiupVQhBuQDw"
+
+docker-compose -f compose-production.yaml run --rm certbot certonly \
+    --manual \
+    --preferred-challenges "dns-01" \
+    --server "https://dv.acme-v02.api.pki.goog/directory" \
+    --domains "hccomputer.net"
